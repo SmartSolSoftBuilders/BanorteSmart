1420720c2b83980b230f068d96304de3183a207b
Horacio Ochoa <hochoa@smartsol.com.mx>
2017-03-10 18:29:19 -0600

    cambio de token

--- Scripts_Fortify/change_token.pl
+++ Scripts_Fortify/change_token.pl
@@ -8,8 +8,12 @@ open($fh, $archivo_conf) or die "no se puede abrir $archivo_conf";
 while (my $row = <$fh>) {
 	chomp $row;
 	#print $row;
-	if (index($row,'token')!=0){
-		$str=$str."$row\n";
+	if ((index($row,'token')!=0)){
+		if((index($row,'Token')!=0)){
+			$str=$str."$row\n";
+		}else{
+			$str=$str."Token=$token\n";
+		}
 	}else{
 		#print $row;
 		$str=$str."token=$token\n";
